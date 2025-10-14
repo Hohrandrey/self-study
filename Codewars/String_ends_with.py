5522610b823843aad338d6fd2f54a5dbae66949d
Hohrandrey <EnderBro_2005@mail.ru>
2025-10-14 13:59:28 +0300

    stepic

--- Codewars/String_ends_with.py
+++ Codewars/String_ends_with.py
@@ -1,5 +1,2 @@
-def solution(text, ending):
-    flag = True
-    if text[-2:] != ending:
-        flag = False
-    print(flag)
+
+
--- Stepic/Программирование на Python/t_2-6/t_2.6.4.py
+++ Stepic/Программирование на Python/t_2-6/t_2.6.4.py
@@ -1,5 +1,5 @@
 start_matr = []
-res_matr = []
+
 
 while True:
     line = input().strip()
@@ -10,3 +10,19 @@ while True:
 
 rows = len(start_matr)
 colums = len(start_matr[0])
+
+res_matr = [[0 for i in range(colums)] for j in range(rows)]
+
+
+for i in range(rows):
+    for j in range(colums):
+        print((i - 1) % rows)
+        print((i + 1) % rows)
+        top = start_matr[(i - 1) % rows][j]
+        bottom = start_matr[(i + 1) % rows][j]
+        left = start_matr[i][(j - 1) % colums]
+        right = start_matr[i][(j + 1) % colums]
+        res_matr[i][j] = top + bottom + left + right
+
+for row in res_matr:
+    print(' '.join(map(str, row)))
