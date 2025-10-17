d200ed922e35c25c39eb7916578ab436b47ea8f7
Hohrandrey <EnderBro_2005@mail.ru>
2025-10-17 09:39:13 +0300

    Update t_3.1.3.py

--- Stepic/Программирование на Python/t_3-1/t_3.1.3.py
+++ Stepic/Программирование на Python/t_3-1/t_3.1.3.py
@@ -1,15 +1,16 @@
 def modify_list(lst):
-    i = 0
-    while i <= len(lst):
-        i+=1
-        if lst[i] % 2 == 0:
-            lst[i] = lst[i] // 2
-        else:
-            lst.remove(lst[i])
+    new_lst = []
+    for num in lst:
+        if num % 2 == 0:
+            new_lst.append(num // 2)
+    lst.clear()
+    lst.extend(new_lst)
 
 
 lst = [1, 2, 3, 4, 5, 6]
 print(modify_list(lst))  # None
 print(lst)               # [1, 2, 3]
+
+lst = [10, 5, 8, 3]
 modify_list(lst)
 print(lst)
