c708dc54b06e6a7b6e7c25c251dc6a3b5d155582
Hohrandrey <EnderBro_2005@mail.ru>
2025-11-04 16:01:30 +0300

    1,,3,2

---
+++ Stepic/Python основы и применение/t_1-3/t_1.3.2.py
@@ -0,0 +1,12 @@
+def s(a, *vs, b=10):
+    res = a + b
+    for v in vs:
+        res += v
+    return res
+
+print(s(11, 10))
+print(s(11, 10, b=10))
+print(s(5, 5, 5, 5, 1))
+
+
+print(s(21),s(11, 10, b=10),s(11, b=20),s(11, 10),s(5, 5, 5, 5, 1))
