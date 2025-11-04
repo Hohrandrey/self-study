6fd2a886acf11ec0f7f9af3f045fad9ba274ffe3
Hohrandrey <84224927+Hohrandrey@users.noreply.github.com>
2025-11-04 23:35:24 +0300

    Update t_1.4.3.py

--- Stepic/Python основы и применение/t_1-4/t_1.4.3.py
+++ Stepic/Python основы и применение/t_1-4/t_1.4.3.py
@@ -1,10 +1,36 @@
-st = { 'global' : {}}
+st = {'global': {"parent": None, "variables": set()}}
+
 
 def create(namespace, parent):
-    pass
+    if namespace not in st:
+        st[namespace] = {"parent": parent, "variables": set()}
+
 
 def add(namespace, var):
-    pass
+    if namespace in st:
+        st[namespace]["variables"].add(var)
+
 
 def get(namespace, var):
-    pass
+    current = namespace
+    while current is not None:
+        if var in st[current]["variables"]:
+            return current
+        current = st[current]["parent"]
+    return None
+
+
+inp = int(input())
+
+for i in range(inp):
+    data = input().split()
+    command = data[0]
+    first = data[1]
+    second = data[2]
+
+    if command == "create":
+        create(first, second)
+    elif command == "add":
+        add(first, second)
+    elif command == "get":
+        print(get(first, second))
