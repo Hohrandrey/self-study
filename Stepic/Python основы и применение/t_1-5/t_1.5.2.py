89c8287346b5e71b886ad5bbfed4da965c5b063a
Hohrandrey <84224927+Hohrandrey@users.noreply.github.com>
2025-11-05 21:54:02 +0300

    1.5

---
+++ Stepic/Python основы и применение/t_1-5/t_1.5.2.py
@@ -0,0 +1,9 @@
+class MoneyBox:
+    def __init__(self, capacity):
+        # конструктор с аргументом – вместимость копилки
+
+    def can_add(self, v):
+        # True, если можно добавить v монет, False иначе
+
+    def add(self, v):
+        # положить v монет в копилку
