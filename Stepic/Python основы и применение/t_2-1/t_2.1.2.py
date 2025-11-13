174f7a6c7ed5ea1710ac7d95e8435a5e01a92ab5
Hohrandrey <EnderBro_2005@mail.ru>
2025-11-13 23:46:41 +0300

    Create t_2.1.2.py

---
+++ Stepic/Python основы и применение/t_2-1/t_2.1.2.py
@@ -0,0 +1,19 @@
+n = int(input())
+iskl_dict = {}
+for _ in range(n):
+    iskl = input().split()
+    classic = iskl[0]
+    if len(iskl) > 2:
+        iskl_dict[iskl[2]] = {'child' : ''}
+        iskl_dict[iskl[2]]['child'] = classic
+    else:
+        iskl_dict[classic] = None
+print(iskl_dict)
+
+m = int(input())
+anton_iskl_sp = []
+for _ in range(m):
+    anton_iskl_sp.append(input())
+
+for elem_sp in anton_iskl_sp:
+    if elem_sp in iskl_dict.keys():
