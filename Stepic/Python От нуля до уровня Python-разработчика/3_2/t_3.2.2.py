677fc481623a1cc6c63755824c7fd704895a3f66
Hohrandrey <84224927+Hohrandrey@users.noreply.github.com>
2025-11-16 23:48:26 +0300

    Update t_3.2.2.py

--- Stepic/Python От нуля до уровня Python-разработчика/3_2/t_3.2.2.py
+++ Stepic/Python От нуля до уровня Python-разработчика/3_2/t_3.2.2.py
@@ -1,14 +1,12 @@
 n = int(input())
 count = 0
 
-if n <=1:
-    print(f'{n} не является простым числом')
-else:
+if  n > 1:
     for i in range(2, int((n**0.5)+1)):
         if n % i == 0:
             count += 1
 
-if count >= 1:
+if count >= 1 or n <=1:
     print(f'{n} не является простым числом')
 else:
     print(f'{n} является простым числом')
