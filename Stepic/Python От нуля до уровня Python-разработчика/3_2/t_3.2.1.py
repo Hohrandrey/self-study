48516b29d7ddbcc8d39b6f3e4d91fb32eaeab77a
Hohrandrey <84224927+Hohrandrey@users.noreply.github.com>
2025-11-16 23:10:35 +0300

    3.2

---
+++ Stepic/Python От нуля до уровня Python-разработчика/3_2/t_3.2.1.py
@@ -0,0 +1,7 @@
+n = int(input())
+res = 1
+
+while n > 0:
+    res *= n
+    n -= 1
+print(res)
---
+++ Stepic/Python От нуля до уровня Python-разработчика/3_2/t_3.2.2.py
@@ -0,0 +1,14 @@
+n = int(input())
+count = 0
+
+if n <=1:
+    print(f'{n} не является простым числом')
+else:
+    for i in range(2, int((n**0.5)+1)):
+        if n % i == 0:
+            count += 1
+
+if count >= 1:
+    print(f'{n} не является простым числом')
+else:
+    print(f'{n} является простым числом')
