551d6edaf159b75dae6ca28f821b3d53aa0c8b58
Hohrandrey <84224927+Hohrandrey@users.noreply.github.com>
2026-03-05 23:57:41 +0300

    Create main.py

---
+++ Stepic/Быстрый старт в FastAPI Python/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+"""
+запуск: uvicorn main:app --reload
+шабло: uvicorn "название файла":"Название функции" --reload
+http://localhost:8000
+"""
