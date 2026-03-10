0e1c3e06bca0465a6281a06f33c60fbe6f390964
Hohrandrey <84224927+Hohrandrey@users.noreply.github.com>
2026-03-10 22:19:13 +0300

    задание базового уровня

--- Stepik/Быстрый старт в FastAPI Python/2_2/app/main.py
+++ Stepik/Быстрый старт в FastAPI Python/2_2/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from .models_app.user_model import User
+
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
@@ -10,3 +13,10 @@ def read_root():
 @app.get("/custom")
 def read_custom_message():
     return {"message": "This is a custom message!"}
+
+
+#задание базового уровня
+User = User(name = "John Doe", id = 1)
+@app.get("/user")
+async def read_user():
+    return User
