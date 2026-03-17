992dcc11e3ac759f12f13f69c1441e7a02337007
Hohrandrey <EnderBro_2005@mail.ru>
2026-03-17 23:24:38 +0300

    fast

--- Stepik/Быстрый старт в FastAPI Python/2_3/app/main.py
+++ Stepik/Быстрый старт в FastAPI Python/2_3/app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
-
+from .models.Feedback import Feedback
 
 app = FastAPI()
 
+users = []
 
 @app.post("/feedback")
-async def feedback():
+async def post(feedback: Feedback):
+    users.append({"name": feedback.name, "message": feedback.message})
+    return {"message": f"Feedback received. Thank you, {feedback.name}."}
+
+@app.get("/users_feed")
+async def show_feedback():
+    return users
