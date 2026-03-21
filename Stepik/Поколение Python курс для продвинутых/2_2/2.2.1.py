10c63c78a34f7c435a2bea01f23a12bb3fcb8a36
Hohrandrey <EnderBro_2005@mail.ru>
2026-03-21 15:12:13 +0300

    Create 2.2.1.py

---
+++ Stepik/Поколение Python курс для продвинутых/2_2/2.2.1.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+chetverti  = [0, 0, 0, 0]
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    if x < 0 < y:
+        chetverti[1] += 1
+    elif x > 0 and y > 0:
+        chetverti[0] += 1
+    elif x < 0 and y < 0:
+        chetverti[2] += 1
+    elif x > 0 > y:
+        chetverti[3] += 1
+
+print(f'Первая четверть: {chetverti[0]}')
+print(f'Вторая четверть: {chetverti[1]}')
+print(f'Третья четверть: {chetverti[2]}')
+print(f'Четвертая четверть: {chetverti[3]}')
