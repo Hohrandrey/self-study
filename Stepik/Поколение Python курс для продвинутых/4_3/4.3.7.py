9cdcf6a43a481e16126610c2c6a3ca4b674e1447
Hohrandrey <84224927+Hohrandrey@users.noreply.github.com>
2026-03-26 18:43:20 +0300

    Update 4.3.7.py

---
+++ Stepik/Поколение Python курс для продвинутых/4_3/4.3.7.py
@@ -0,0 +1,10 @@
+string = input().split()
+rash = 1
+res = [[]]
+
+while rash <= len(string):
+    for i in range(len(string)):
+        if i + rash <= len(string):
+            res.append(string[i:i + rash])
+    rash += 1
+print(res)
