720e5f538c61264678e63b29241e128c81ae6598
Hohrandrey <84224927+Hohrandrey@users.noreply.github.com>
2026-03-26 16:56:10 +0300

    4.3.6

---
+++ Stepik/Поколение Python курс для продвинутых/4_3/4.3.6.py
@@ -0,0 +1,10 @@
+string = input().split()
+n = int(input())
+
+def chunked(string, n):
+    chanks = []
+    for i in range(0, len(string), n):
+        chanks.append(string[i:i+n])
+    return chanks
+
+print(chunked(string, n))
