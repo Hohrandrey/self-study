5b91d37bdce511a1f132d601d3c27a9834f18136
Hohrandrey <EnderBro_2005@mail.ru>
2026-04-17 13:39:58 +0300

    Create 4.4.6.py

---
+++ Stepik/Поколение Python курс для продвинутых/4_4/4.4.6.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+sp = []
+for i in range(n):
+    sp.append(list(map(int, input().split())))
+
+max_elem = -10000000000
+
+for i in range(n):
+    for j in range(n):
+        if j <= i <= n - 1-j or j >= i >= n-1-j:
+            max_elem = max(sp[i][j], max_elem)
+
+print(max_elem)
