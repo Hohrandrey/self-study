2bb5bbfb2b69c1600e0833cc90021e695c78c109
Hohrandrey <EnderBro_2005@mail.ru>
2026-04-18 14:05:20 +0300

    Create 4.5.1.py

---
+++ Stepik/Поколение Python курс для продвинутых/4_5/4.5.1.py
@@ -0,0 +1,14 @@
+n = int(input())
+m = int(input())
+
+matrix = [[0] * m for _ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        matrix[i][j] = i * j
+
+for i in range(n):
+    for j in range(m):
+        out = str(matrix[i][j]).ljust(3)
+        print(out, end='')
+    print()
