217072995923ca0ce368e600a37556accf2b2a71
Hohrandrey <84224927+Hohrandrey@users.noreply.github.com>
2026-04-21 22:34:20 +0300

    Update 4.5.7.py

--- Stepik/Поколение Python курс для продвинутых/4_5/4.5.7.py
+++ Stepik/Поколение Python курс для продвинутых/4_5/4.5.7.py
@@ -1,13 +1,15 @@
 n = int(input())
 
 matrix = []
+res_matrix = [[0 for _ in range(n)]for _ in range(n)]
 
 for _ in range(n):
     matrix.append(list(map(int, input().split())))
 
+
 for i in range(n):
     for j in range(n):
-        matrix[i][j], matrix[j][n-1-i] = matrix[j][n-1-i], matrix[i][j]
+        res_matrix[j][n-1-i] = matrix[i][j]
 
-for line in matrix:
+for line in res_matrix:
     print(*line)
