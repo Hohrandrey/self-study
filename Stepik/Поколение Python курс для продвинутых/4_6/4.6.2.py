97e7b11eda1719448cb2f89d2478b66dda23c069
Hohrandrey <EnderBro_2005@mail.ru>
2026-04-26 11:44:34 +0300

    4.6.2-4.6.3

---
+++ Stepik/Поколение Python курс для продвинутых/4_6/4.6.2.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+matrix = [[0 for _ in range(n)] for i in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if i + j == n -1:
+            matrix[i][j] = "1"
+        elif  i + j >= n - 1:
+            matrix[i][j] = "2"
+
+for line in matrix:
+    print(*line)
---
+++ Stepik/Поколение Python курс для продвинутых/4_6/4.6.3.py
@@ -0,0 +1,15 @@
+n ,m = map(int, input().split())
+
+matrix = []
+cur_elem  = 1
+
+for i in range(n):
+    line = []
+    for j in range(m):
+        line.append(cur_elem)
+        cur_elem += 1
+    matrix.append(line)
+
+
+for line in matrix:
+    print(*line)
