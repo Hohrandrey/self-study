83662265d34ac4d525109f1a6bdfbbaa37814a41
Hohrandrey <EnderBro_2005@mail.ru>
2026-04-27 11:41:05 +0300

    4.6.4-4.6.6

---
+++ Stepik/Поколение Python курс для продвинутых/4_6/4.6.4.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+
+matrix = [[0 for _ in range(m)] for _ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        matrix[i][j] = n*j + i + 1
+        print(str(matrix[i][j]).ljust(3), end = '')
+    print()
