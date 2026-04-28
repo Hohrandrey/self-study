9f5329e7f31096e86d1b0f48bef56c3d862334eb
Hohrandrey <EnderBro_2005@mail.ru>
2026-04-28 19:47:40 +0300

    Update main.py

--- Stepik/Быстрый старт в FastAPI Python/3_3/app/main.py
+++ Stepik/Быстрый старт в FastAPI Python/3_3/app/main.py
@@ -1,3 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, HTTPException
 
 app = FastAPI()
+
+
+@app.get("/headers")
+def get_headers(user_agent: str | None = Header(None), accept_language: str | None = Header(None)):
+    if not user_agent or not accept_language:
+        raise HTTPException(status_code=<int>, detail="<str>")
+    return {"User-Agent" : user_agent, "Accept-Language" : accept_language}
