ae863ed798de4e70529f110a61aad5f79d41c4d7
Hohrandrey <EnderBro_2005@mail.ru>
2026-05-07 11:44:07 +0300

    Create 4.7.5.py

---
+++ Stepik/Поколение Python курс для продвинутых/4_7/4.7.5.py
@@ -0,0 +1,16 @@
+A = [[1, 0], [4, 1]]
+
+def multiply_matrices(X, Y):
+    result = [[0, 0], [0, 0]]
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                result[i][j] += X[i][k] * Y[k][j]
+    return result
+
+result = [[1, 0], [4, 1]]
+
+for _ in range(24):
+    result = multiply_matrices(result, A)
+
+print(result)
