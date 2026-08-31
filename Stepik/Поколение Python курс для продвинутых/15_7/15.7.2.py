a208c9214b173ba9a2fe23242a6b482753aaba4d
Hohrandrey <84224927+Hohrandrey@users.noreply.github.com>
2026-08-31 14:30:21 +0300

    15,7

---
+++ Stepik/Поколение Python курс для продвинутых/15_7/15.7.2.py
@@ -0,0 +1,12 @@
+from functools import reduce
+
+data = [
+    ['Bangkok', 18180280, 'admin'], ['Karachi', 21422590, 'primary'],
+    ['Bengaluru', 13187098, 'primary'],
+]
+
+filtered_data = filter(lambda town: town[1] > 10000000 and town[2] == 'primary', data)
+sorted_filtered_data = list(sorted(filtered_data, key = lambda town: town[0]))
+res_data = reduce(lambda string, town: string + town[0] + ', ', sorted_filtered_data, 'Cities: ')
+
+print(res_data[:-2])
