95a4d22862ae02dbacd32f2d29e0f393d2def7b4
Hohrandrey <EnderBro_2005@mail.ru>
2026-09-04 14:58:40 +0300

    t-start

--- T-start/Septemder 2026/3.py
+++ T-start/Septemder 2026/3.py
@@ -16,4 +16,30 @@
 n, t = map(int, input().split())
 a = map(int, input().split())
 matrix = [[n*(x-1)+y for y in range(1, n+1)] for x in range(1, n+1)]
-print(matrix)
+
+def check(matrix):
+    for row in matrix:
+        if all(cell == 'x' for cell in row):
+            return True
+
+        # Проверка столбцов
+    for j in range(n):
+        if all(matrix[i][j] == 'x' for i in range(n)):
+            return True
+
+        # Проверка главной диагонали
+    if all(matrix[i][i] == 'x' for i in range(n)):
+        return True
+
+        # Проверка побочной диагонали
+    if all(matrix[i][n - 1 - i] == 'x' for i in range(n)):
+        return True
+
+    return False
+
+for el in a:
+    for i in range(n):
+        for j in range(n):
+            if matrix[i][j] == el:
+                matrix[i][j] = 'x'
+    print(check(matrix))
