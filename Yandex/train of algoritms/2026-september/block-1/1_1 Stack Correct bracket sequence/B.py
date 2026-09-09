80d99224d2dade165798f93288498765b9323d46
Hohrandrey <84224927+Hohrandrey@users.noreply.github.com>
2026-09-09 22:30:56 +0300

    Update B.py

--- Yandex/train of algoritms/2026-september/block-1/1_1 Stack Correct bracket sequence/B.py
+++ Yandex/train of algoritms/2026-september/block-1/1_1 Stack Correct bracket sequence/B.py
@@ -1,8 +1,3 @@
-import sys
-
-from fontTools.misc.plistlib import end_string
-
-
 def main():
     dict_of_brackets_front = {'(': ')', '[': ']', '{': '}'}
     dict_of_brackets_backs = {')': '(', ']': '[', '}': '{'}
@@ -18,20 +13,15 @@ def main():
         end_string = ''
 
         while len(start_string) != n/2:
-            """
-            добивать строку основываясь на приоритете до половины длины
-            
-            if w[0] in dict_of_brackets_backs:
-                if start_string[-1] in dict_of_brackets_front:
+
+            if start_string[-1] in dict_of_brackets_backs:
+                if w[0] in dict_of_brackets_front:
                     start_string += w[0]
                 else:
                     start_string += dict_of_brackets_backs[w[0]]
-            else:
-                if start_string[-1] in dict_of_brackets_front:
-                    start_string += dict_of_brackets_front[w[0]]
-                else:
-                    start_string += w[0]
-            """
+            elif start_string[-1] in dict_of_brackets_front:
+                start_string += w[0]
+
 
         for el in start_string:
             if el in dict_of_brackets_backs:
@@ -41,9 +31,6 @@ def main():
 
         res_string = start_string + end_string[::-1]
         return res_string
-
-
-
 
 
 if __name__ == '__main__':
