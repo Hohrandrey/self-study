d26bce80b6778e8c898ca0b0353f5471393db537
Hohrandrey <84224927+Hohrandrey@users.noreply.github.com>
2026-09-13 21:52:31 +0300

    Update E. Meaning of an Arithmetic Expression.py

--- Yandex/train of algoritms/2026-september/block-1/1_2 Infix and postfix notation/E. Meaning of an Arithmetic Expression.py
+++ Yandex/train of algoritms/2026-september/block-1/1_2 Infix and postfix notation/E. Meaning of an Arithmetic Expression.py
@@ -2,43 +2,66 @@ def main():
     string = input()
     dict_of_priority = {'+':0, '-':0, '*':1, '/':1, '(':-1}
     def create_postfix(string):
-        res = ''
+        res = []
         stack_of_expressions=[]
-        for elem in string:
-            if elem in '+-*/()':
-                if elem in '+-*/':
-                    if len(stack_of_expressions) == 0:
-                        stack_of_expressions.append(elem)
-                    else:
-                        while stack_of_expressions and dict_of_priority[stack_of_expressions[-1]] >= dict_of_priority[elem]:
-                            res += stack_of_expressions.pop()
-                        stack_of_expressions.append(elem)
-                elif elem == '(':
+        i = 0
+        n = len(string)
+        while i < n:
+            elem = string[i]
+            if elem in '+-*/':
+                if not stack_of_expressions:
                     stack_of_expressions.append(elem)
-                elif elem == ')':
+                    i += 1
+                else:
+                    while stack_of_expressions and dict_of_priority[stack_of_expressions[-1]] >= dict_of_priority[elem]:
+                        res.append(stack_of_expressions.pop())
+                    stack_of_expressions.append(elem)
+                    i += 1
+            elif elem == '(':
+                stack_of_expressions.append(elem)
+                i += 1
+            elif elem == ')':
+                if '(' in stack_of_expressions:
                     while stack_of_expressions and stack_of_expressions[-1] != '(':
-                        res += stack_of_expressions.pop()
+                        res.append(stack_of_expressions.pop())
                     stack_of_expressions.pop()
+                    i += 1
+                else:
+                    return 'WRONG'
             elif elem.isdigit():
-                res+=elem
+                j = i
+                while j < n and string[j].isdigit():
+                    j += 1
+                num_str = string[i:j]
+                val = int(num_str)
+                res.append(val)
+                i = j
             elif elem == ' ':
-                continue
+                i += 1
             else:
                 return 'WRONG'
+
+        if '(' in stack_of_expressions:
+            return 'WRONG'
+
         while stack_of_expressions:
-            res += stack_of_expressions.pop()
+            res.append(stack_of_expressions.pop())
         return res
 
     cr_string = create_postfix(string)
     if cr_string == 'WRONG':
         print('WRONG')
         return
+
     stack = []
 
     for elem in cr_string:
-        if elem.isdigit():
-            stack.append(int(elem))
+        if isinstance(elem, int):
+            stack.append(elem)
         else:
+            if len(stack) < 2:
+                print('WRONG')
+                return
             op2 = stack.pop()
             op1 = stack.pop()
             match elem:
@@ -49,7 +72,10 @@ def main():
                 case '*':
                     stack.append(op1 * op2)
                 case '/':
-                    stack.append(op1 / op2)
+                    if op2 == 0:
+                        print('WRONG')
+                        return
+                    stack.append(op1 // op2)
 
     if len(stack) == 1:
         print(stack.pop())
