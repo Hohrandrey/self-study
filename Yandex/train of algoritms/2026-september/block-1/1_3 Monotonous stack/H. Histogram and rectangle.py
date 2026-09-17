7f9686d83f94bd417b6101f9b1d79d7fd6d56e80
Hohrandrey <EnderBro_2005@mail.ru>
2026-09-17 14:08:24 +0300

    Update H. Histogram and rectangle.py

--- Yandex/train of algoritms/2026-september/block-1/1_3 Monotonous stack/H. Histogram and rectangle.py
+++ Yandex/train of algoritms/2026-september/block-1/1_3 Monotonous stack/H. Histogram and rectangle.py
@@ -3,6 +3,14 @@ def main():
     n = data[0]
     row = data[1:n+1]
     stack = []
+    min_gist = max(n * min(row), max(row))
+    for index, rectangle in enumerate(row):
+        if not stack:
+            stack.append((index, rectangle))
+        else:
+            stack.append((index, rectangle))
+    print(min_gist)
+    print(stack)
 
 
 if __name__ == '__main__':
